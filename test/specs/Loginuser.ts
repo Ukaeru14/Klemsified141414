61dfba6b938da37351a542b6bf43a6a2002d63c9
Clement Ukaeru <clement.ukaeru@ii.co.uk>
2025-09-24 10:09:37 +0100

    faker update

--- test/specs/Loginuser.ts
+++
@@ -1,7 +0,0 @@
-import Login from "../pageobjects/loginginpage.ts"; 
-describe("Automation Exercise - Login", () => {
-    
-    it("should log in successfully with valid credentials", async () => {
-        await Login.loginToAccount();
-    })
-})
--- test/specs/autosignup.ts
+++
@@ -1,7 +0,0 @@
-import autosignup from '../pageobjects/autoexersignuppage.ts';
-describe('automation signup', () =>{
-  it('should sign up successfully', async () =>{
-    await autosignup.autosignup();
- 
-});
-});
